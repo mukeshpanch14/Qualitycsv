a1b5453665efc3b2779929045e5ebfed91e76217
sarthakdey <sarthakdey@outlook.com>
2017-12-12 08:48:26 -0500

    edited

--- Code/ReadModule.py
+++ Code/ReadModule.py
@@ -8,11 +8,27 @@ class ReadModule(object):
     self.header_count = header_count
     self.footer_count = footer_count
     self.delimiter = delimiter
+    
+    with open(self.filename) as file:
+        i=0
+        for line in file.readlines():
+          i=i+1
+        self.count=i
       
     if(self.header_count>0):
       self.header_pos = self.header_count-1 #gets the header position
     else:
       self.header_pos = -1
+    
+    if(self.footer_count>0):
+      self.footer_pos=self.count - self.footer_count
+    else:
+      self.footer_pos = self.count
+    print(self.footer_pos)
+    #if(self.footer_count>0):
+     # self.footer_pos = self.footer_count-1 #gets the footer position
+    #else:
+    #  self.footer_pos = -1
     
   def __str__(self):
     return ('filename is '+ '"' + self.filename+'"' + ' with delimiter : '+'"' + self.delimiter + '"')
@@ -28,20 +44,24 @@ class ReadModule(object):
           i=i+1
       self.header = header_string.split(self.delimiter)
     return self.header
-    
-  def read_file(self):
+  
+  def read_file(self): #edit
     df = pd.DataFrame()
     with open(self.filename) as file:
-      reader = csv.reader(file, delimiter=',', quotechar="'")
+      reader = csv.reader(file, delimiter=self.delimiter, quotechar="'")
       i=0
       for row in reader:
-        if i!=self.header_pos:
+        #print(self.footer_pos)
+        if i>self.header_pos and i<self.footer_pos:
           df = df.append(pd.Series(row),ignore_index=True)
         i=i+1
     print(df)
     
-a = ReadModule('file.csv',0,0,',')
+a = ReadModule('file.csv',2,2,',')
 #print(a)
 header = a.header()
-print(header)
+#print(header)
+#a.count()
 a.read_file()
+
+
--- Halder/test2.py
+++ Halder/test2.py
@@ -1,14 +1,14 @@
 import csv
 import pandas as pd
-def parse(filename):
-    
-    df = pd.read_csv(filename)
-    #df.to_csv(filename, header=None)
-    print df.to_csv(header=None,index=False)
-    
 
-def main():
-    parse('EMP_MUKESH_new.csv')
+def read_funding_data(path):
+    with open(path, 'rU') as data:
+        data.readline()            # Skip the header
+        reader = csv.reader(data)  # Create a regular tuple reader
+        for row in map(FundingRecord.parse, reader):
+            yield row
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    for row in read_funding_data(EMP_MUKESH_new.csv):
+        print row
+        break
