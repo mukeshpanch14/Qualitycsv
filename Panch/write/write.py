216cba7b20e4e6dd33f38fa454e887a6ba2c8cdb
mukeshpanch14 <mukesh.panch14@gmail.com>
2018-01-09 12:32:10 +0000

    ad

--- Panch/write/write.py
+++ Panch/write/write.py
@@ -1,9 +1,26 @@
 import csv
-file_name='write_out.csv'
+import os
 
-with open(file_name,'a+') as file:
-    sniff=csv.Sniffer()
-    has_header = sniff.has_header(file.read(2048))
-    print(has_reader)
+
+header_data=[['Rule Id','Run Date','Run Time','Subject Area','Rule Name','Rule Sub Ctgy','Rule Description','Attribute Name','File Name','Success Flag','Result Flag','Error Reason']]
+myData=[[123,'04101995','05:58:20','Inventory','First Rule','1Sub Ctgy','Description','Attrib','File_name',1,123,'No Error']]
+
+file_name='write_out.csv'
+t=os.path.isfile('write_out.csv')
+print(t)
+
+if t:
+    print("Fijle Present")
+    myFile=open(file_name,'a')
+    writer = csv.writer(myFile)
+    writer.writerows(myData)
+else:
+    print("File Absent")
+    myFile=open(file_name,'a')
+    writer = csv.writer(myFile)
+    writer.writerows(header_data)
+    writer.writerows(myData)
+
     
-    
+
+
