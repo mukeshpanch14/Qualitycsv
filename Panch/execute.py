3ef634a059d2580c55d284e72a9ab8cfb8389a11
mukeshpanch14 <mukesh.panch14@gmail.com>
2018-02-21 10:50:41 +0000

    as

---
+++ Panch/execute.py
@@ -0,0 +1,20 @@
+import ReadModule as RM
+#import NullCheck as NC
+import DuplicateCheck as D
+#import pandas as pd
+#import csv
+import HeaderChecker as H
+
+#a = RM.ReadModule('file.csv',1,0,',','\'')
+a = RM.ReadModule('file.csv',1,0,',','\'')
+header = a.header_func()
+df = a.read_file(['Emp_Id','Emp_Name','City','DOB'])
+#print('**************************')
+#print(df)
+#print('**************************')
+#rule_nullcheck = NC.NullCheck(df,['Emp_Name'])
+#rule_nullcheck.checkNull()
+dup=D.Duplicate(df,col_list=['Emp_Name','City'])
+dup.duplicate()
+hc=H.Header(['Emp_Id','Emp_Name','City','DOB'],header)
+hc.HeaderCheck()
